05449a546ae8b064d3a4c2f9b5c1f7d1ffdef920
Marco Osorio <mosorior173@gmail.com>
2022-11-30 08:11:39 +0100

    Ejercicios 01 al 19

--- Cadenas/Ej01to10.java
+++ Cadenas/Ej01to10.java
@@ -1,14 +1,12 @@
-
-
-
 public class Ej01to10{
 
     /*---------------------------------- NÚMERO ESPACIOS ----------------------------*/
     public static int numeroEspacios(String x){
-        int longi = x.length;
+        int longi = x.length();
+        char y[] = x.toCharArray();
         int espacios = 0;
         for (int i = 0; i < longi; i++){
-            if(x.charAt(i)= " "){
+            if(y[i]== ' '){
                 espacios++;
             }
         }
@@ -16,11 +14,12 @@ public class Ej01to10{
    }
 
 /*---------------------------------- NÚMERO VOCALES ----------------------------*/
-   public static int numeroVocales(String x){
-        int longi = x.length;
+   public static int numeroVocales(String y){
+        int longi = y.length();
+        char x[] = y.toCharArray();
         int vocales = 0;
         for(int i = 0; i < longi; i++){
-            if (x.charAt(i) = "a" || x.charAt(i) = "e" || x.charAt(i) = "i" || x.charAt(i) = "o" || x.charAt(i) = "u"){
+            if (x[i] == 'a' || x[i] == 'e' || x[i] == 'i' || x[i] == 'o' || x[i] == 'u'){
                 vocales++;
             }
         }
@@ -29,13 +28,13 @@ public class Ej01to10{
 
 /*---------------------------------- ES PALÍNDROMO ----------------------------*/
    public static boolean esPalindromo(String x){
-    int longi = x.length;
+    int longi = x.length();
     boolean palindromo = false;
     if(longi == 1){
         return true;
     }
     else{
-        for(i = 0; i<longi; i++){
+        for(int i = 0; i<longi; i++){
             if(x.charAt(i) == x.charAt(longi-i)){
                 palindromo = true;
             }
@@ -49,7 +48,7 @@ public class Ej01to10{
 
    /*---------------------------------- CONTIENE ----------------------------*/
    public static boolean contiene(String x, char y){
-       int longi= x.length;
+       int longi= x.length();
        for(int i = 0; i < longi; i++){
         if(x.charAt(i) == y){
             return true;
@@ -61,34 +60,36 @@ public class Ej01to10{
    /*---------------------------------- REPITE CARACTER----------------------------*/
    
    public static String repiteCaracter(char x, int y){
-    String cadena;
-    cadena[0] = x;  
+    char cadena[] = new char[y]; 
     for(int i = 0; i<y; i++){
-        cadena.concat(cadena[0])
+        cadena[i] +=  x;
     }
-    return cadena;
+    String repiteCaracter = cadena.toString();
+    return repiteCaracter;
    }
 
    /*---------------------------------- PAD LEFT ----------------------------*/
 
    public static String padLeft(String x, int longi){
-        String cadenaEspacios[];
-        cadenaEspacios[0]=" ";
+        String padLeft = "";
+        char cadenaEspacios[] = new char[longi];
+        cadenaEspacios[0]=' ';
         for(int i = 0; i < longi; i++){
-            cadenaEspacios[i]= "";
+            cadenaEspacios[i]= ' ';
         }
-        padLeft = cadenaEspacios + x;
+        padLeft = padLeft.valueOf(cadenaEspacios) + x;
         return padLeft;
     }
    
    /*---------------------------------- PAD RIGHT ----------------------------*/
 
    public static String padRight(String x, int longi){
-        String cadenaEspacios[];
-        for(int i = x.length; i < longi; i++){
-            cadenaEspacios[i] = " ";
+        String padRight = "";
+        char cadenaEspacios[] = new char[longi];
+        for(int i = x.length(); i < longi; i++){
+            cadenaEspacios[i] = ' ';
         }
-        padRight = x + cadenaEspacios;
+        padRight = x + padRight.valueOf(cadenaEspacios);
         return padRight;
     }
 
@@ -96,14 +97,18 @@ public class Ej01to10{
 /*---------------------------------- QUITA ESPACIOS ----------------------------*/
    public static String quitaEspacios(String x) {
         x.trim();
-        String quitaEspacios[];
+        char quitaEspacio[] = new char[100];
+        quitaEspacio[0] = ' ';
+        String quitaEspacios = "";
+        char y[] = x.toCharArray();
         int j = 0;
-      for(int i = 0; i< x.length; i++){
-        if(x[i] != 0){
-            quitaEspacios[j] = x[i];
-            j++
+      for(int i = 0; i< x.length(); i++){
+        if(y[i] != ' '){
+            quitaEspacio[j] = y[i];
+            j++;
         }
       }
+      quitaEspacios = quitaEspacios.valueOf(quitaEspacio);
       return quitaEspacios;
    }
 
@@ -112,41 +117,43 @@ public class Ej01to10{
    public static String quitaEspaciosTrim(String x) {
         int numSI= 0;
         int numSF = 0;
-        String espaciosI[];
-        String espaciosF[];
+        char y[] = x.toCharArray();
+        char espaciosI[];
+        char espaciosF[];
         int j = 0;
+        char espaciosQuitados[];
         String quitaEspaciosTrim[];
-        for(int i = 0; i < x.length; i++){
-            if(x[i] == " "){
+        for(int i = 0; i < x.length(); i++){
+            if(y[i] ==' '){
                 numSI++;
             }
-            if (x[i] != " "){
+            if (y[i] != ' '){
                 break;
             }
         }
-        for(int i = x.length; i > numSI; i--){
-            if(x[i] == " "){
+        for(int i = x.length(); i > numSI; i--){
+            if(y[i] == ' '){
                 numSF++;
             }
-            if (x[i] != " "){
+            if (y[i] != ' '){
                 break;
             }
         }
-        for(int i = numSI; i < (x.length-numSF); i++){
-            if(x[i] != " "){
-                espaciosQuitados[j] = x[i];
+        for(int i = numSI; i < (x.length()-numSF); i++){
+            if(y[i] != ' '){
+                espaciosQuitados[j] = y[i];
                 j++;
             }
         }
 
         for(int i = 0; i < numSI; i++){
-            espaciosI[i] = " ";
+            espaciosI[i] = ' ';
         }
         for(int i = 0; i < numSF; i++){
-            espaciosF[i] = " ";
+            espaciosF[i] = ' ';
         }
 
-        String espaciosQuitados = espaciosI + espaciosQuitados + espaciosF;
+        quitaEspaciosTrim = espaciosI.toString() + espaciosQuitados + espaciosF;
         
 
 
@@ -156,12 +163,14 @@ public class Ej01to10{
 /*------------------------------------- SUSTITUYE CARACTER ----------------------------- */
 
 public static String sustituyeCaracter(String x, char a, char b) {
-    for(int i = 0; i < x.length; i++){
-        if( x[i] == a){
-            x[i] = b;
+    char y[] = x.toCharArray();
+    for(int i = 0; i < x.length(); i++){
+        if( y[i] == a){
+            y[i] = b;
         }
     }
-    return x;
+    String sustituyeCaracter = y.toString();
+    return sustituyeCaracter;
  }
  
  /*----------------------------------- CUENTA PALABRAS ------------------------------- */
@@ -169,18 +178,19 @@ public static String sustituyeCaracter(String x, char a, char b) {
    public static int cuentaPalabras(String x) {
     boolean stop = false;
     int j = 0;
+    char y[] = x.toCharArray();
     int cuentaPalabras = 0;
     while(!stop){
-        if(x[j] != " "){
-            j++
-            stop = true
+        if(y[j] != ' '){
+            j++;
+            stop = true;
         }
         else{
-            stop = false
+            stop = false;
         }
     }
-    for(int i = j; i < x.length; i++){
-        if(x[i] == " "){
+    for(int i = j; i < x.length(); i++){
+        if(y[i] == ' '){
             cuentaPalabras++;
         }
     }
---
+++ Cadenas/Ej11to20.java
@@ -0,0 +1,155 @@
+public class Ej11to20{
+
+    /*---------------------------------- ES NUMERO ----------------------------*/
+    public static boolean esNumero(String x){
+      char y[] = x.toCharArray();
+      int counter = 0;
+      for(int i = 0; i < x.length(); i++){
+        if(y[i] == '0' || y[i] == '1'|| y[i] == '2' || y[i] == '3' || y[i] == '4' || y[i] == '5' || y[i] == '6' || y[i] == '7' || y[i] == '8' || y[i] == '9' ){
+            counter++;
+        }
+      }
+      if(x.length() == counter){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
+    /*--------------------------------- QUITA CARACTER ---------------------- */
+
+    public static String quitaCaracter(String x, char a){
+        String quitaCaracter;
+        char[] y = x.toCharArray();
+        char[] aux = new char[x.length()];
+        int j = 0;
+        for(int i = 0; i < x.length(); i++){
+            if(y[i] != a){
+                aux[j] = y[i];
+                j++;
+            }
+        }
+
+        quitaCaracter = aux.toString();
+        return quitaCaracter;
+    }
+
+    /*--------------------------------- QUITA ACENTOS --------------------- */
+    public static String quitaAcentos(String x){
+        char[] y = new char[x.length()];
+
+        for(int i = 0; i < x.length(); i++){
+            if(y[i] == 'á' || y[i] == 'à'){
+                y[i] = 'a';
+            }
+            if(y[i] == 'è' || y[i] == 'é'){
+                y[i] = 'e';
+            }
+            if(y[i] == 'ì' || y[i] == 'í'){
+                y[i] = 'i';
+            }
+            if(y[i] == 'ò' || y[i] == 'ó' ){
+                y[i] = 'o';
+            }
+
+            if(y[i] == 'ù' || y[i] == 'ú'){
+                y[i] = 'u';
+            }
+
+        }
+        String quitaAcentos = y.toString();
+
+        return quitaAcentos;
+    }
+
+
+    /*--------------------------------- INVIERTE CADENA --------------------- */
+    public static String invierteCadena(String x){
+        char[] y = x.toCharArray();
+        char[] aux = new char[x.length()];
+        int j = x.length();
+        for(int i = 0; i < x.length(); i++){
+            aux[i] = y[j];
+            j--;
+        }
+
+        String invierteCadena = aux.toString();
+
+        return invierteCadena;
+
+    }
+
+    /*---------------------------------- VECES CARACTER ------------------------- */
+    public static int vecesCaracter(String x, char a){
+        char[] y = x.toCharArray();
+        int vecesCaracter = 0;
+        for(int i = 0; i < x.length(); i++){
+            if(y[i] == a){
+                vecesCaracter++;
+            }
+        }
+        return vecesCaracter;
+    }
+    /* ----------------------------------VECES PALABRA --------------------------- */
+
+    public static int vecesPalabra(String x, String a){
+        String[] y = x.split(" ");
+        int vecesPalabra = 0;
+        for(int i = 0; i < x.length(); i++){
+            if(y[i] == a){
+                vecesPalabra++;
+            }
+        }
+        return vecesPalabra;
+        
+    }
+
+    /*------------------------------------ MAYÚSCULAS PRIMERA --------------------- */
+    public static String mayusculasPrimera(String x){
+        char[] y = x.toCharArray();
+        char aux;
+
+        if (y[0] != ' '){
+            y[0] = Character.toUpperCase(y[0] );
+          }
+        
+        for(int i = 0; i < x.length(); i++){
+            if( y[i] == ' ' && y[i+1] >= 97 && y[i+1] <= 122){
+                y[i+1] = Character.toUpperCase(y[i+1]);
+            }
+        }
+
+        String mayusculasPrimera = y.toString();
+
+        return mayusculasPrimera;
+    }
+
+    /*---------------------------------- SUSTITUYE PALABRA -------------------------- */
+    public static String sustituyePalabra(String x, String a, String b){
+        String[] y = x.split(" ");
+        String sustituyePalabra = " ";
+        for(int i = 0; i < x.length(); i++){
+            if(y[i] == a){
+                y[i] = b;
+            }
+            sustituyePalabra = sustituyePalabra +" "+y[i];
+
+        }
+    
+        return sustituyePalabra;
+        
+    }
+    /*--------------------------------- QUITA ESPACIOS SOBRANTES ---------------------- */
+    public static String quitaEspaciosSobrantes(String x){
+        String[] y = x.split(" ");
+        String quitaEspaciosSobrantes = " ";
+        for(int i = 0; i < x.length(); i++){
+            if(y[i] == " "){
+                
+            }
+        }
+    }
+
+
+}
